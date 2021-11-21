54607809bca1ddec462c42de47d30810de05538d
Piotr Wilczynski <delwing@gmail.com>
2021-11-21 22:30:47 +0100

    try to checkout

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-if [ $2 == "" ]
+if [[ "$2" == "" ]]
 then
+    git status
     git checkout $GITHUB_BASE_REF $1
     node /index.js $1 $1 $2 $3
 else
+    git status
     git checkout $GITHUB_BASE_REF $2
     node /index.js $1 $2 $3 $4
 fi
