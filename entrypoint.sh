6514e5a68a6443e5833f2d7ed7cb9f540aaecc27
Piotr Wilczynski <delwing@gmail.com>
2021-11-21 22:52:16 +0100

    check branches

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,13 +2,11 @@
 
 if [[ "$2" == "" ]]
 then
-    ls
-    git status
+    git branch -a
     git checkout $GITHUB_BASE_REF -- $1
     node /index.js $1 $1 $3 $4
 else
-    ls
-    git status
+    git branch -a
     git checkout $GITHUB_BASE_REF -- $2
     node /index.js $1 $2 $3 $4
 fi
